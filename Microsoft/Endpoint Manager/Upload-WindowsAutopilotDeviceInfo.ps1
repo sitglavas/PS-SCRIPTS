b64a2179e02c2d67b850661717a5964fa9e73535
sitglavas <117894594+sitglavas@users.noreply.github.com>
2023-12-06 14:39:51 +0100

    Delete Microsoft/Endpoint Manager/Upload-WindowsAutopilotDeviceInfo.ps1

--- Microsoft/Endpoint Manager/Upload-WindowsAutopilotDeviceInfo.ps1
+++
@@ -1,8 +0,0 @@
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
-
-Install-Script Upload-WindowsAutopilotDeviceInfo
-
-#fill in the variables
-$tenantname = ""
-$grouptag = ""
-Upload-WindowsAutopilotDeviceInfo -TenantName $tenantname -GroupTag $grouptag -Verbose
