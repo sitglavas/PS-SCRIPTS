0c1dea09b9682aab6681a8cde7e79f7d2863fd9c
sitglavas <117894594+sitglavas@users.noreply.github.com>
2023-12-06 14:39:22 +0100

    Delete Microsoft/Disable_RestrictDriverInstallationToAdministrators.ps1

--- Microsoft/Disable_RestrictDriverInstallationToAdministrators.ps1
+++
@@ -1,7 +0,0 @@
-# Check if the key exists, if not, create it
-if (-not (Test-Path "HKLM:\Software\Policies\Microsoft\Windows NT\Printers\PointAndPrint")) {
-    New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows NT\Printers\PointAndPrint" -Force
-}
-
-# Set the value
-Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows NT\Printers\PointAndPrint" -Name "RestrictDriverInstallationToAdministrators" -Value 0 -Type DWord -Force
